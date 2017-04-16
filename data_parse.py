c3ed44ff496e6fff969013776878fac85d282d08
Victor Pryazhnikov <victor@pryazhnikov.com>
2017-04-16 16:58:08 +0300

    Codestyle fixes accorting to PEP8 requirements

--- data_parse.py
+++ data_parse.py
@@ -10,25 +10,25 @@ import sys
 
 class FundLoader:
     column_codes = {
-        '1'      : ('Объем на начало периода', 'AmountTotalBeforeRub'),
-        '1.1.'   : ('Поступления', 'IncomeTotalRub'),
-        '1.1.1.' : ('в т.ч. доходы от размещения', 'IncomeEarningsRub'),
-        '1.2.'   : ('Изъятия', 'WithdrawalTotalRub'),
-        '1.3.'   : ('Курсовая разница от переоценки активов в иностранной валюте', 'CurrencyRateDiffRub'),
-        '2'      : ('Объем на конец периода', 'AmountTotalAfterRub'),
-        '2.1.'   : ('в т.ч. в процентах к ВВП', 'AmountTotalAfterGDPPercent'),
-        '3'      : ('Справочно: ', np.nan),
-        '3.1.'   : ('Объем средств фонда на конец периода (млрд. долларов США)', 'AmountTotalAfterUsd'),
-        '3.2.'   : ('Доходы от размещения, зачисленные в федеральный бюджет', 'AmountIncomeBudget'),
-        '3.3.'   : ('Расчетная сумма дохода от размещения на счетах в Банке России на конец периода', 'AmountIncomeCbr'),
-        '4'      : ('Валютная структура средств фонда на счетах в Банке России на конец периода (в соответствующей валюте)**', np.nan),
-        '4.1.'   : ('доллары США', 'CurrencyUsdAfter'),
-        '4.2.'   : ('евро', 'CurrencyEurAfter'),
-        '4.3.'   : ('фунты стерлингов', 'CurrencyGbpAfter'),
-        '4.4.'   : ('рубли', 'CurrencyRubAfter'),
-        '5'      : ('Размещено в иные разрешенные активы на конец периода', 'OtherTotalAfter'),
-        '5.1.'   : ('рубли', 'OtherRubAfter'),
-        '5.2.'   : ('иностранная валюта', 'OtherForeignAfter'),
+        '1':      ('Объем на начало периода', 'AmountTotalBeforeRub'),
+        '1.1.':   ('Поступления', 'IncomeTotalRub'),
+        '1.1.1.': ('в т.ч. доходы от размещения', 'IncomeEarningsRub'),
+        '1.2.':   ('Изъятия', 'WithdrawalTotalRub'),
+        '1.3.':   ('Курсовая разница от переоценки активов в иностранной валюте', 'CurrencyRateDiffRub'),
+        '2':      ('Объем на конец периода', 'AmountTotalAfterRub'),
+        '2.1.':   ('в т.ч. в процентах к ВВП', 'AmountTotalAfterGDPPercent'),
+        '3':      ('Справочно: ', np.nan),
+        '3.1.':   ('Объем средств фонда на конец периода (млрд. долларов США)', 'AmountTotalAfterUsd'),
+        '3.2.':   ('Доходы от размещения, зачисленные в федеральный бюджет', 'AmountIncomeBudget'),
+        '3.3.':   ('Расчетная сумма дохода от размещения на счетах в Банке России на конец периода', 'AmountIncomeCbr'),
+        '4':      ('Валютная структура средств фонда на счетах в Банке России на конец периода (в соответствующей валюте)**', np.nan),
+        '4.1.':   ('доллары США', 'CurrencyUsdAfter'),
+        '4.2.':   ('евро', 'CurrencyEurAfter'),
+        '4.3.':   ('фунты стерлингов', 'CurrencyGbpAfter'),
+        '4.4.':   ('рубли', 'CurrencyRubAfter'),
+        '5':      ('Размещено в иные разрешенные активы на конец периода', 'OtherTotalAfter'),
+        '5.1.':   ('рубли', 'OtherRubAfter'),
+        '5.2.':   ('иностранная валюта', 'OtherForeignAfter'),
     }
 
     def __init__(self, file_name):
@@ -41,17 +41,17 @@ class FundLoader:
         return {k: v[1] for k, v in self.column_codes.items()}
 
     def verify_headers(self, df):
-        df.copy().rename(columns={0:'code', 1:'value'})
+        df.copy().rename(columns={0: 'code', 1: 'value'})
         input_columns = self._columns_input_names()
         for index, row in df.iterrows():
             if row['Code'] in self.column_codes:
-                expected_value = input_columns[ row['Code'] ]
+                expected_value = input_columns[row['Code']]
                 if expected_value != row['Attribute']:
                     raise NameError('Attribute #{} has value "{}" ("{}" is expected)'.format(row['Code'], row['Attribute'], expected_value))
 
     def verify_values(self, df):
         # Размер фонда на начало периода должен быть равен размеру на конец предыдущего
-        border_amounts_df = df[ ['AmountTotalBeforeRub', 'AmountTotalAfterRub'] ]
+        border_amounts_df = df[['AmountTotalBeforeRub', 'AmountTotalAfterRub']]
         last_after_amount = None
         for key, series in border_amounts_df.iterrows():
             if last_after_amount and (series['AmountTotalBeforeRub'] != last_after_amount):
@@ -63,7 +63,7 @@ class FundLoader:
 
         # * 30 января 2008 г. средства Стабилизационного фонда были зачислены на счета Федерального казначейства
         # в Банке России по учету средств Резервного фонда и Фонда национального благосостояния.
-        input_df.rename(columns = {'Январь 2008*':'2008-01-30'}, inplace = True)
+        input_df.rename(columns={'Январь 2008*': '2008-01-30'}, inplace=True)
         input_df.dropna(thresh=100, inplace=True)
 
         # Проверка структуры human readable показателей
@@ -93,12 +93,14 @@ class FundLoader:
 
         return result_df
 
+
 def get_sort_date(name):
     match = re.search(r'(\d{2})-(\d{2})-(\d{4})', name)
     if match:
         return match.group(3) + '-' + match.group(2) + '-' + match.group(1)
     else:
         return None
+
 
 def get_last_source_file(pattern, offset):
     abs_path = os.path.dirname(os.path.abspath(__file__))
@@ -107,9 +109,11 @@ def get_last_source_file(pattern, offset):
     sorted_files_list = sorted(files_list, key=get_sort_date)
     return sorted_files_list[-1 - offset]
 
+
 def get_target_file_name(basename):
     abs_path = os.path.dirname(os.path.abspath(__file__))
     return abs_path + '/data/output/' + os.path.basename(basename)
+
 
 def main():
     pd.set_option('display.width', 160)
